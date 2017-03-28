962b99db574ebab25361f05c894226f6f0238606
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2017-03-28 12:15:49 +0200

    implemented code to traverse and draw all floor edges

--- src/client/viewer.components/Viewing.Extension.Fader/Viewing.Extension.Fader.js
+++ src/client/viewer.components/Viewing.Extension.Fader/Viewing.Extension.Fader.js
@@ -59,6 +59,10 @@ class FaderExtension extends ExtensionBase {
 
     //this.wallIds=[] // initialise in onGeometryLoaded
 
+    _lineMaterial = createLineMaterial();
+
+    _vertexMaterial = createVertexMaterial();
+
     return true
   }
 
@@ -154,10 +158,105 @@ class FaderExtension extends ExtensionBase {
     const fragIds = await Toolkit.getFragIds(this.viewer.model, data.dbId)
     console.log(fragIds)
 
-    var floor_mesh = fragIds.map((fragId) => {
+    var floor_mesh_fragment = fragIds.map((fragId) => {
       return this.viewer.impl.getFragmentProxy(this.viewer.model, fragId)
     })
-    console.log(floor_mesh)
+    console.log(floor_mesh_fragment)
+
+    // in Philippe's Autodesk.ADN.Viewing.Extension.MeshData.js
+    // function drawMeshData, the fragment proxy is ignored and 
+    // the render proxy is used instead:
+
+    var floor_mesh_render = fragIds.map((fragId) => {
+      return this.viewer.impl.getRenderProxy(this.viewer.model, fragId)
+    })
+    console.log(floor_mesh_render)
+
+    floor_mesh_render = floor_mesh_render[0]
+
+    var matrix = floor_mesh_render.matrixWorld;
+
+    var geometry = floor_mesh_render.geometry;
+
+    //not working
+    //geometry.applyMatrix(matrix);
+
+    var attributes = geometry.attributes;
+
+    var vA = new THREE.Vector3();
+    var vB = new THREE.Vector3();
+    var vC = new THREE.Vector3();
+
+    if (attributes.index !== undefined) {
+
+      var indices = attributes.index.array || geometry.ib;
+      var positions = geometry.vb ? geometry.vb : attributes.position.array;
+      var stride = geometry.vb ? geometry.vbstride : 3;
+      var offsets = geometry.offsets;
+
+      if (!offsets || offsets.length === 0) {
+
+        offsets = [{start: 0, count: indices.length, index: 0}];
+      }
+
+      for (var oi = 0, ol = offsets.length; oi < ol; ++oi) {
+
+        var start = offsets[oi].start;
+        var count = offsets[oi].count;
+        var index = offsets[oi].index;
+
+        for (var i = start, il = start + count; i < il; i += 3) {
+
+          var a = index + indices[i];
+          var b = index + indices[i + 1];
+          var c = index + indices[i + 2];
+
+          vA.fromArray(positions, a * stride);
+          vB.fromArray(positions, b * stride);
+          vC.fromArray(positions, c * stride);
+
+          vA.applyMatrix4(matrix);
+          vB.applyMatrix4(matrix);
+          vC.applyMatrix4(matrix);
+
+          this.drawVertex (vA, 0.05);
+          this.drawVertex (vB, 0.05);
+          this.drawVertex (vC, 0.05);
+
+          this.drawLine(vA, vB);
+          this.drawLine(vB, vC);
+          this.drawLine(vC, vA);
+        }
+      }
+    }
+    else {
+
+      var positions = geometry.vb ? geometry.vb : attributes.position.array;
+      var stride = geometry.vb ? geometry.vbstride : 3;
+
+      for (var i = 0, j = 0, il = positions.length; i < il; i += 3, j += 9) {
+
+        var a = i;
+        var b = i + 1;
+        var c = i + 2;
+
+        vA.fromArray(positions, a * stride);
+        vB.fromArray(positions, b * stride);
+        vC.fromArray(positions, c * stride);
+
+        vA.applyMatrix4(matrix);
+        vB.applyMatrix4(matrix);
+        vC.applyMatrix4(matrix);
+
+        this.drawVertex (vA, 0.05);
+        this.drawVertex (vB, 0.05);
+        this.drawVertex (vC, 0.05);
+
+        this.drawLine(vA, vB);
+        this.drawLine(vB, vC);
+        this.drawLine(vC, vA);
+      }
+    }    
 
     // from floor mesh, access all faces and use only those wwith the same normal
 
@@ -177,6 +276,76 @@ class FaderExtension extends ExtensionBase {
       viewer.impl.invalidate(true);
     }
   }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  // vertex material
+  //
+  ///////////////////////////////////////////////////////////////////////////
+  createVertexMaterial() {
+
+    var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+
+    viewer.impl.matman().addMaterial(
+      'adn-material-vertex',
+      material,
+      true);
+
+    return material;
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // line material
+  //
+  ///////////////////////////////////////////////////////////////////////////
+  createLineMaterial() {
+
+    var material = new THREE.LineBasicMaterial({
+      color: 0x0000ff,
+      linewidth: 2
+    });
+
+    viewer.impl.matman().addMaterial(
+      'adn-material-line',
+      material,
+      true);
+
+    return material;
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // draw a line
+  //
+  ///////////////////////////////////////////////////////////////////////////
+  drawLine(start, end) {
+
+    var geometry = new THREE.Geometry();
+
+    geometry.vertices.push(new THREE.Vector3(
+      start.x, start.y, start.z));
+
+    geometry.vertices.push(new THREE.Vector3(
+      end.x, end.y, end.z));
+
+    var line = new THREE.Line(geometry, this._lineMaterial);
+
+    this.viewer.impl.scene.add(line);
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // draw a vertex
+  //
+  ///////////////////////////////////////////////////////////////////////////
+  drawVertex (v, radius) {
+
+    var vertex = new THREE.Mesh(
+      new THREE.SphereGeometry(radius, 20),
+      this._vertexMaterial);
+
+    vertex.position.set(v.x, v.y, v.z);
+
+    this.viewer.impl.scene.add(vertex);
+  }  
 }
 
 Autodesk.Viewing.theExtensionManager.registerExtension(
